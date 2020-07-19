284187cfdbead91df0eb4ae24c7252d0b10a0477
NayanaBannur <42090597+NayanaBannur@users.noreply.github.com>
2020-07-19 15:23:20 +0530

    Add m2 plots. Update all plots.

--- src/utils/plotting.py
+++ src/utils/plotting.py
@@ -1,8 +1,11 @@
 import json
 from copy import deepcopy
-from datetime import datetime, timedelta
 
+import numpy as np
 import pandas as pd
+import seaborn as sns
+from adjustText import adjust_text
+from entities.forecast_variables import ForecastVariable
 from matplotlib import pyplot as plt, dates as mdates
 from modules.data_fetcher_module import DataFetcherModule
 
@@ -22,8 +25,6 @@ plot_labels = {
     "deceased": "Deceased cases"
 }
 
-all_variables = ['hospitalised', 'deceased', 'recovered', 'confirmed']
-
 
 def plot_format(ax):
     ax.xaxis.set_major_locator(mdates.DayLocator(interval=7))
@@ -34,21 +35,7 @@ def plot_format(ax):
 def plot_vertical_lines(ax, vertical_lines):
     if vertical_lines is not None:
         for line in vertical_lines:
-            ax.axvline(x=line['date'], ls=':', color=line['color'], label=line['label'])
-
-
-def get_observations_for_plot(region_type, region_name, start_date, end_date, data_source, smooth=False, filepath=None):
-
-    df_actual = DataFetcherModule.get_observations_for_region(region_type, region_name,
-                                                              data_source=data_source, smooth=smooth, filepath=filepath)
-    df_actual = df_actual.set_index('observation')
-    df_actual = df_actual.transpose().reset_index()
-    start = df_actual.index[df_actual['date'] == start_date].tolist()[0]
-    end = df_actual.index[df_actual['date'] == end_date].tolist()[0]
-    df_actual = df_actual[start: end + 1]
-    df_actual['date'] = pd.to_datetime(df_actual['date'])
-
-    return df_actual
+            ax.axvline(x=pd.to_datetime(line['date']), ls=':', color=line['color'], label=line['label'])
 
 
 def plot_data(region, region_type, data_source=None, plot_config='plot_config.json', plot_name='default.png'):
@@ -72,7 +59,7 @@ def plot_data(region, region_type, data_source=None, plot_config='plot_config.js
     fig, ax = plt.subplots(figsize=(16, 12))
 
     for variable in plot_variables:
-        plt.plot(actual['index'], actual[variable], plot_markers['observed'],
+        ax.plot(actual['index'], actual[variable].values, plot_markers['observed'],
                  color=plot_colors[variable], label=plot_labels[variable])
 
     plot_format(ax)
@@ -86,14 +73,16 @@ def plot_data(region, region_type, data_source=None, plot_config='plot_config.js
     plt.savefig(plot_name)
 
 
-def multivariate_case_count_plot(df_actual, df_smoothed=None, df_predictions=None, variables=None,
-                                 column_label='Predictions', column_tag='', vertical_lines=None, title='', path=None):
+def multivariate_case_count_plot(df_actual, df_smoothed=None, df_predictions_train=None, df_predictions_test=None,
+                                 variables=None, column_label='Predicted mean', column_tag='mean', vertical_lines=None,
+                                 title='', path=None):
     """Creates a plot of one or more variables and types of data
 
     Args:
         df_actual (pd.DataFrame): actual observations
         df_smoothed (pd.DataFrame, optional): smoothed observations (default: None)
-        df_predictions (pd.DataFrame, optional): predictions (default: None)
+        df_predictions_train (pd.DataFrame, optional): predictions for train interval (default: None)
+        df_predictions_test (pd.DataFrame, optional): predictions for test interval (default: None)
         variables (list, optional): list of variables to plot (default: None)
         column_label (str, optional): label for type of column such as mean, percentile (default: 'Predictions')
         column_tag (str, optional): tag indicating column from df_predictions to be plotted (default: '')
@@ -106,21 +95,24 @@ def multivariate_case_count_plot(df_actual, df_smoothed=None, df_predictions=Non
 
     # By default, plot all variables
     if variables is None:
-        variables = all_variables
+        variables = [ForecastVariable.hospitalized.name, ForecastVariable.recovered.name,
+                     ForecastVariable.deceased.name, ForecastVariable.confirmed.name]
 
     fig, ax = plt.subplots(figsize=(16, 12))
 
     # Plot actual observations, smoothed observations and predictions
     for variable in variables:
-        plt.plot(df_actual['date'], df_actual[variable], '-o',
+        plt.plot(df_actual['index'], df_actual[variable], '-o',
                  color=plot_colors[variable], label=f'Observed: {plot_labels[variable]}')
         if df_smoothed is not None:
-            plt.plot(df_smoothed['date'], df_smoothed[variable], '-',
+            plt.plot(df_smoothed['index'], df_smoothed[variable], '-',
                      color=plot_colors[variable], label=f'Smoothed: {plot_labels[variable]}')
-        if df_predictions is not None:
-            column = '_'.join([variable, column_tag])
-            plt.plot(df_predictions['date'], df_predictions[column], '--',
-                     color=plot_colors[variable], label=f'{column_label}: {plot_labels[variable]}')
+        for df_predictions in [df_predictions_train, df_predictions_test]:
+            if df_predictions is not None:
+                column = '_'.join([variable, column_tag])
+                if column in df_predictions.columns:
+                    plt.plot(df_predictions['date'], df_predictions[column], '--',
+                             color=plot_colors[variable], label=f'{column_label}: {plot_labels[variable]}')
 
     # Plot vertical lines to mark specific events
     plot_vertical_lines(ax, vertical_lines)
@@ -131,22 +123,25 @@ def multivariate_case_count_plot(df_actual, df_smoothed=None, df_predictions=Non
     plt.ylabel('Case counts')
     plt.xlabel('Time')
     plt.xticks(rotation=45)
-    plt.legend()
+    handles, labels = plt.gca().get_legend_handles_labels()
+    legend_dict = dict(zip(labels, handles))
+    plt.legend(legend_dict.values(), legend_dict.keys())
     plt.grid()
 
     if path is not None:
         plt.savefig(path)
 
 
-def single_variable_case_count_plot(variable, df_actual, df_smoothed=None, df_predictions=None, column_tags=None,
-                                    vertical_lines=None, title='', path=''):
+def single_variable_case_count_plot(variable, df_actual, df_smoothed=None, df_predictions_train=None,
+                                    df_predictions_test=None, column_tags=None, vertical_lines=None, title='', path=''):
     """Creates a plot for a single variable
 
     Args:
         variable (str): variable to plot
         df_actual (pd.DataFrame): actual observations
         df_smoothed (pd.DataFrame, optional): smoothed observations (default: None)
-        df_predictions (pd.DataFrame, optional): predictions (default: None)
+        df_predictions_train (pd.DataFrame, optional): predictions for train interval (default: None)
+        df_predictions_test (pd.DataFrame, optional): predictions for test interval (default: None)
         column_tags (list, optional): tags indicating column from df_predictions to be plotted (default: None)
         vertical_lines (list, optional): list of dict of vertical lines to be included in the plot of the form
             [{'date': date, 'color': color, 'label': label}]
@@ -158,21 +153,33 @@ def single_variable_case_count_plot(variable, df_actual, df_smoothed=None, df_pr
     fig, ax = plt.subplots(figsize=(16, 12))
 
     # Plot actual observations, smoothed observations and predictions
-    plt.plot(df_actual['date'], df_actual[variable], '-o',
-             color=plot_colors[variable], label=f'Observed: {plot_labels[variable]}')
+    plt.plot(df_actual['index'], df_actual[variable], '-o',
+             color=plot_colors[variable], label=f'Observed')
     if df_smoothed is not None:
-        plt.plot(df_smoothed['date'], df_smoothed[variable], '-',
-                 color=plot_colors[variable], label=f'Smoothed: {plot_labels[variable]}')
-    if df_predictions is not None and column_tags is not None:
+        plt.plot(df_smoothed['index'], df_smoothed[variable], '-',
+                 color=plot_colors[variable], label=f'Smoothed')
+
+    if df_predictions_train is not None:
+        plt.plot(df_predictions_train['date'], df_predictions_train[f'{variable}_mean'], 'x',
+                 color=plot_colors[variable], label='Predicted mean')
+
+    percentile_labels = []
+
+    if df_predictions_test is not None and column_tags is not None:
         for tag in column_tags:
             column = '_'.join([variable, tag])
-            if tag == 'mean':
-                plt.plot(df_predictions['date'], df_predictions[column], 'x',
-                         color=plot_colors[variable], label=f'Predicted mean: {plot_labels[variable]}')
-            else:
-                plt.plot(df_predictions['date'], df_predictions[column], '--',
-                         color=plot_colors[variable], label=f'Predicted percentile: {plot_labels[variable]}')
-            # Label the percentile curves
+            if column in df_predictions_test.columns:
+                if tag == 'mean':
+                    plt.plot(df_predictions_test['date'], df_predictions_test[column], 'x',
+                             color=plot_colors[variable], label='Predicted mean')
+                else:
+                    plt.plot(df_predictions_test['date'], df_predictions_test[column], '--',
+                             color=plot_colors[variable], label=f'Predicted percentiles')
+                percentile_labels.append(plt.text(
+                    x=df_predictions_test['date'].iloc[-1],
+                    y=df_predictions_test[column].iloc[-1], s=tag))
+
+    adjust_text(percentile_labels, arrowprops=dict(arrowstyle="->", color='b', lw=1))
 
     # Plot vertical lines to mark specific events
     plot_vertical_lines(ax, vertical_lines)
@@ -183,53 +190,133 @@ def single_variable_case_count_plot(variable, df_actual, df_smoothed=None, df_pr
     plt.ylabel('Case counts')
     plt.xlabel('Time')
     plt.xticks(rotation=45)
-    plt.legend()
+    handles, labels = plt.gca().get_legend_handles_labels()
+    legend_dict = dict(zip(labels, handles))
+    plt.legend(legend_dict.values(), legend_dict.keys())
     plt.grid()
 
     if path is not None:
         plt.savefig(path)
 
 
-def m1_plots(df_predictions, train1_model_params,
-             train1_run_day, train1_start_date, train1_end_date,
-             test_run_day, test_start_date, test_end_date, forecast_config):
+def m1_plots(region_name, df_actual, df_smoothed, df_predictions_train, df_predictions_test,
+             train1_start_date, test1_start_date, column_tags=None, variables=None):
+    """Creates all M1 plots
 
-    # TODO: Do we fetch actuals and get forecasts here or outside
+    Args:
+        region_name (str): name of region
+        df_actual (pd.DataFrame): actual observations
+        df_smoothed (pd.DataFrame): smoothed observations
+        df_predictions_train (pd.DataFrame): predictions for train interval
+        df_predictions_test (pd.DataFrame): predictions for test interval
+        train1_start_date (str): start date for train1 interval
+        test1_start_date (str): start date for test1 interval
+        column_tags (list, optional): tags indicating column from df_predictions to be plotted (default: None)
+        variables (list, optional): list of variables to plot (default: None)
 
-    # Start and end dates of plots
-    plot_start_date = (datetime.strptime(train1_start_date, "%m/%d/%y") - timedelta(days=7)).strftime("%-m/%-d/%y")
-    plot_end_date = test_end_date
+    """
 
-    # Get actual observations and smoothed data
-    df_actual = get_observations_for_plot(train1_model_params['region_type'], train1_model_params['region'],
-                                          plot_start_date, plot_end_date,
-                                          train1_model_params['data_source'], smooth=False,
-                                          filepath=train1_model_params['input_filepath'])
-    df_smoothed = get_observations_for_plot(train1_model_params['region_type'], train1_model_params['region'],
-                                            plot_start_date, plot_end_date,
-                                            train1_model_params['data_source'], smooth=True,
-                                            filepath=train1_model_params['input_filepath'])
+    # By default, plot all variables
+    if variables is None:
+        variables = [ForecastVariable.hospitalized.name, ForecastVariable.recovered.name,
+                     ForecastVariable.deceased.name, ForecastVariable.confirmed.name]
 
     # Train start and test start markers in plots
     vertical_lines = [
         {'date': train1_start_date, 'color': 'brown', 'label': 'Train starts'},
-        {'date': test_start_date, 'color': 'black', 'label': 'Test starts'}
+        {'date': test1_start_date, 'color': 'black', 'label': 'Test starts'}
     ]
 
-    region_name = " ".join(train1_model_params['region_name'])
-
     # Multivariate plot with M1 mean predictions
-    multivariate_case_count_plot(df_actual, df_smoothed=df_smoothed, df_predictions=df_predictions,
-                                 column_label='mean', column_tag='mean', vertical_lines=vertical_lines,
-                                 title=f'{region_name}: M1 fit')
-
-    # Get percentiles to plot
-    uncertainty_params = forecast_config['model_parameters']['uncertainty_parameters']
-    percentiles = uncertainty_params['percentiles'] + uncertainty_params['ci']
-    percentiles = [str(i) for i in percentiles]
+    multivariate_case_count_plot(df_actual, df_smoothed=df_smoothed,
+                                 df_predictions_train=df_predictions_train, df_predictions_test=df_predictions_test,
+                                 variables=variables, column_label='mean', column_tag='mean',
+                                 vertical_lines=vertical_lines, title=f'{region_name}: M1 fit')
 
     # Single variable plot
-    for variable in all_variables:
-        single_variable_case_count_plot(variable, df_actual, df_smoothed=df_smoothed, df_predictions=df_predictions,
-                                        column_tags=percentiles, vertical_lines=vertical_lines,
-                                        title=f'{region_name}: M1 fit - {variable}')
+    for variable in variables:
+        single_variable_case_count_plot(variable, df_actual, df_smoothed=df_smoothed,
+                                        df_predictions_train=df_predictions_train,
+                                        df_predictions_test=df_predictions_test, column_tags=column_tags,
+                                        vertical_lines=vertical_lines, title=f'{region_name}: M1 fit - {variable}')
+
+
+def m2_plots(region_name, df_actual, df_smoothed, df_predictions_train, train2_start_date, column_tags=None,
+             variables=None):
+    """Creates all M2 plots
+
+    Args:
+        region_name (str): name of region
+        df_actual (pd.DataFrame): actual observations
+        df_smoothed (pd.DataFrame): smoothed observations
+        df_predictions_train (pd.DataFrame): predictions for train interval
+        train2_start_date (str): start date for train2 interval
+        column_tags (list, optional): tags indicating column from df_predictions to be plotted (default: None)
+        variables (list, optional): list of variables to plot (default: None)
+
+    """
+
+    # By default, plot all variables
+    if variables is None:
+        variables = [ForecastVariable.hospitalized.name, ForecastVariable.recovered.name,
+                     ForecastVariable.deceased.name, ForecastVariable.confirmed.name]
+
+    # Train start and test start markers in plots
+    vertical_lines = [{'date': train2_start_date, 'color': 'brown', 'label': 'Train starts'}]
+
+    # Multivariate plot with M1 mean predictions
+    multivariate_case_count_plot(df_actual, df_smoothed=df_smoothed,
+                                 df_predictions_train=df_predictions_train, df_predictions_test=None,
+                                 variables=variables, column_label='mean', column_tag='mean',
+                                 vertical_lines=vertical_lines, title=f'{region_name}: M2 fit')
+
+    # Single variable plot
+    for variable in variables:
+        single_variable_case_count_plot(variable, df_actual, df_smoothed=df_smoothed,
+                                        df_predictions_train=df_predictions_train, df_predictions_test=None,
+                                        column_tags=column_tags, vertical_lines=vertical_lines,
+                                        title=f'{region_name}: M2 fit - {variable}')
+
+
+def m2_forecast_plots(region_name, df_actual, df_smoothed, df_predictions_train, df_predictions_forecast,
+                      train2_start_date, forecast_start_date, column_tags=None, variables=None):
+    """Creates all M1 plots
+
+    Args:
+        region_name (str): name of region
+        df_actual (pd.DataFrame): actual observations
+        df_smoothed (pd.DataFrame): smoothed observations
+        df_predictions_train (pd.DataFrame): predictions for train interval
+        df_predictions_forecast (pd.DataFrame): predictions for forecast interval
+        train2_start_date (str): start date for train2 interval
+        forecast_start_date (str): start date for forecast interval
+        column_tags (list, optional): tags indicating column from df_predictions to be plotted (default: None)
+        variables (list, optional): list of variables to plot (default: None)
+
+    """
+
+    # By default, plot all variables
+    if variables is None:
+        variables = [ForecastVariable.hospitalized.name, ForecastVariable.recovered.name,
+                     ForecastVariable.deceased.name, ForecastVariable.confirmed.name]
+
+    # Train start and forecast start markers in plots
+    vertical_lines = [
+        {'date': train2_start_date, 'color': 'brown', 'label': 'Train starts'},
+        {'date': forecast_start_date, 'color': 'black', 'label': 'Forecast starts'}
+    ]
+
+    # Multivariate plot with M1 mean predictions
+    multivariate_case_count_plot(df_actual, df_smoothed=df_smoothed,
+                                 df_predictions_train=df_predictions_train, df_predictions_test=df_predictions_forecast,
+                                 variables=variables, column_label='mean', column_tag='mean',
+                                 vertical_lines=vertical_lines, title=f'{region_name}: M2 forecast')
+
+    # Single variable plot
+    for variable in variables:
+        single_variable_case_count_plot(variable, df_actual, df_smoothed=df_smoothed,
+                                        df_predictions_train=df_predictions_train,
+                                        df_predictions_test=df_predictions_forecast, column_tags=column_tags,
+                                        vertical_lines=vertical_lines,
+                                        title=f'{region_name}: M2 forecast - {variable}')
+
