5b6851bc0b22d3c5d0ed4b1409202df54a5ca33a
NayanaBannur <42090597+NayanaBannur@users.noreply.github.com>
2020-07-22 13:48:36 +0530

    Placeholder function for fetching staffing ratios

--- src/modules/data_fetcher_module.py
+++ src/modules/data_fetcher_module.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from entities.data_source import DataSource
 from data_fetchers.data_fetcher_factory import DataFetcherFactory
 
@@ -34,6 +36,10 @@ class DataFetcherModule(object):
         data_fetcher = DataFetcherFactory.get_data_fetcher(data_source)
         metadata = data_fetcher.get_regional_metadata(region_type, region_name, filepath)
         return metadata
+
+    @staticmethod
+    def get_staffing_ratios(filepath):
+        return pd.read_csv(filepath)
 
 
 if __name__ == "__main__":
